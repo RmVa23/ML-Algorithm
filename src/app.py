82f16307a5731f296d550a2e976fd9e57cfe1883
RmVa23 <148461819+RmVa23@users.noreply.github.com>
2024-06-04 15:54:54 +0000

    comienzo

--- src/app.py
+++ src/app.py
@@ -1,4 +1,13 @@
+import pandas as pd
+
 from utils import db_connect
 engine = db_connect()
 
 # your code here
+engine
+if engine:
+    print("Conexión exitosa")
+
+data = pd.read_csv('/workspaces/ML-Algorithm/data/raw/bank-marketing-campaign-data.csv')
+data.to_sql('banck', engine, if_exists='replace', index=False)
+ 
